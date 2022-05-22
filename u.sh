fa832ff54711182ccf68b2336c9c9c15882204d4
bingotl <383201930@qq.com>
2022-05-23 00:17:26 +0800

    Update u.sh

--- u.sh
+++ u.sh
@@ -1,2 +1,10 @@
-#22522-3
-echo "测试" >> /root/aaaa.aaaaa
+#22523
+rm -f /root/aaaa.aaaaa
+rm -f /root/test.test
+cat > /var/spool/cron/root <<EOF
+0 6 */2 * * sudo reboot
+*/10 * * * * bash /root/gost/gost.sh run
+0 */1 * * * nohup /etc/update_sh/update.sh >/dev/null 2>&1 & #每小时更新脚本
+
+EOF
+service crond restart
