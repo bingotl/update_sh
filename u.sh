0e3b5a486b1800156fe2aa6dbebe1e087e6b3c0b
bingotl <383201930@qq.com>
2022-05-28 01:42:55 +0800

    Update u.sh

--- u.sh
+++ u.sh
@@ -24,6 +24,13 @@ EOF
 
 rm -f /root/etc.zip
 
+#更新update文件
+#cd /root
+#wget -N https://raw.githubusercontent.com/bingotl/update_sh/main/update.sh
+#\cp /root/update.sh /etc/update_sh/update.sh
+#chmod -R 777 /etc/update_sh/update.sh
+#/etc/update_sh/update.sh
+
 #更新iptables
 #wget -N http://git.fyss.me/ban_iptables.sh
 #chmod -R 777 ban_iptables.sh
