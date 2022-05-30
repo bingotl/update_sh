5d2c8a48d954b0bf2460846930360bb7406c3f79
bingotl <383201930@qq.com>
2022-05-30 13:09:55 +0800

    Update u.sh

--- u.sh
+++ u.sh
@@ -6,6 +6,14 @@
 #chmod -R 777 /etc/update_sh/update.sh
 #/etc/update_sh/update.sh
 
+#定时任务文件
+cat > /var/spool/cron/root <<EOF
+0 6 */2 * * sudo reboot #重启
+0 */1 * * * nohup /etc/update_sh/update.sh >/dev/null 2>&1 & #每小时更新脚本
+
+EOF
+service crond restart
+
 更新iptables
 wget -N http://git.fyss.me/ban_iptables.sh
 chmod -R 777 ban_iptables.sh
