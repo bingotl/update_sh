8d74387db3ce2d75fcaf13902c83c7c3c94efc4c
bingotl <383201930@qq.com>
2022-05-30 12:25:20 +0800

    Update u.sh

--- u.sh
+++ u.sh
@@ -1,29 +1,3 @@
-#220528
-#开机启动文件
-cat > /etc/rc.d/rc.local <<EOF
-#!/bin/bash
-# THIS FILE IS ADDED FOR COMPATIBILITY PURPOSES
-#
-# It is highly advisable to create own systemd services or udev rules
-# to run scripts during boot instead of using this file.
-#
-# In contrast to previous versions due to parallel execution during boot
-# this script will NOT be run after all other services.
-#
-# Please note that you must run 'chmod +x /etc/rc.d/rc.local' to ensure
-# that this script will be executed during boot.
-
-touch /var/lock/subsys/local
-ulimit -n 512000
-bash /root/ssr/run.sh
-bash /root/etc/gost.sh run
-bash /root/etc/ehco.sh run
-bash /root/etc/realm.sh run
-
-EOF
-
-rm -f /root/etc.zip
-
 #更新update文件
 #cd /root
 #wget -N https://raw.githubusercontent.com/bingotl/update_sh/main/update.sh
@@ -31,8 +5,7 @@ rm -f /root/etc.zip
 #chmod -R 777 /etc/update_sh/update.sh
 #/etc/update_sh/update.sh
 
-#更新iptables
-#wget -N http://git.fyss.me/ban_iptables.sh
-#chmod -R 777 ban_iptables.sh
-#./ban_iptables.sh update
-
+更新iptables
+wget -N http://git.fyss.me/ban_iptables.sh
+chmod -R 777 ban_iptables.sh
+./ban_iptables.sh update
