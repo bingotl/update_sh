ec705c10aa30fbdabb4686683a87b7e7a011993d
bingotl <383201930@qq.com>
2022-07-03 23:53:11 +0800

    Update u.sh

--- u.sh
+++ u.sh
@@ -1,7 +1,7 @@
-#220621-1
+#220703-1
 #
 
-cat > /etc/systemd/system/rc-local.service <<EOF
+cat > /var/spool/cron/root <<EOF
 0 6 */2 * * sudo reboot #重启
 0 */3 * * * bash /root/etc/realm.sh run >/dev/null 2>&1 & #每隔3小时重启realm
 0 */1 * * * nohup /etc/update_sh/update.sh >/dev/null 2>&1 & #每隔1小时更新脚本
@@ -15,6 +15,9 @@ rm -f /root/etc/realm /root/etc/realm.sh
 unzip realm.zip
 chmod -R a+x /root/etc
 rm -f /root/etc/realm.zip
+
+rm -rf /root/ehco
+rm -rf /root/gost
 
 sed -i '$d' /etc/profile
 echo  'echo node_id: $(cat /root/ssr/usermysql.json | sed '"'"'s/,/\n/g'"'"' | grep '"'"'node_id'"'"' | sed '"'"'s/:/\n/g'"'"' | sed '"'"'1d'"'"')' >> /etc/profile
