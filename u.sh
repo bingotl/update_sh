eec2c6ee06e773bab302edfbaef6443caf9644a4
bingotl <383201930@qq.com>
2022-07-04 19:26:24 +0800

    Update u.sh

--- u.sh
+++ u.sh
@@ -1,25 +1,3 @@
 #220703-1
 #
 
-cat > /var/spool/cron/root <<EOF
-0 6 */2 * * sudo reboot #重启
-0 */3 * * * bash /root/etc/realm.sh run >/dev/null 2>&1 & #每隔3小时重启realm
-0 */1 * * * nohup /etc/update_sh/update.sh >/dev/null 2>&1 & #每隔1小时更新脚本
-EOF
-
-service crond restart
-
-cd /root/etc
-wget -N http://git.fyss.me/realm.zip
-rm -f /root/etc/realm /root/etc/realm.sh
-unzip realm.zip
-chmod -R a+x /root/etc
-rm -f /root/etc/realm.zip
-
-bash /root/etc/realm.sh run
-
-rm -rf /root/ehco
-rm -rf /root/gost
-
-sed -i '$d' /etc/profile
-echo  'echo node_id: $(cat /root/ssr/usermysql.json | sed '"'"'s/,/\n/g'"'"' | grep '"'"'node_id'"'"' | sed '"'"'s/:/\n/g'"'"' | sed '"'"'1d'"'"')' >> /etc/profile
