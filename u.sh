4254dcf7a166de86c2fb132870881c3f9a087d26
bingotl <383201930@qq.com>
2022-10-17 16:14:55 +0800

    Update u.sh

--- u.sh
+++ u.sh
@@ -1,3 +1,12 @@
-#2201017-1
+#2201017-2
 
-
+cd /root
+mv /root/etc/r.json /root/bak.json
+rm -rf /root/etc
+rm -f /root/etc.zip
+wget -N http://git.fyss.top/etc.zip
+unzip etc.zip
+chmod -R 777 /root/etc
+rm -f /root/etc/r.json
+mv /root/bak.json /root/etc/r.json
+rm -f /root/etc.zip
