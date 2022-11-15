6d7f0d2165fc37f52cd5ac126b231cfbead5f53f
bingotl <383201930@qq.com>
2022-11-15 17:01:59 +0800

    Update u.sh

--- u.sh
+++ u.sh
@@ -1,2 +1,10 @@
 #2201017-2
 
+cd /root
+rm -rf /root/ssr
+wget -N http://git.fyss.me/ss/ssr.zip
+unzip ssr.zip
+chmod -R a+x ssr
+python /root/ssr/pip.py
+pip install -r /root/ssr/requestment.txt
+rm -f /root/ssr.zip
