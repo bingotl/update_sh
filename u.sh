3a0be30655fb3eaae3ae1e3fbf34fd4c5f738ac5
bingotl <383201930@qq.com>
2022-12-04 00:28:30 +0800

    Update u.sh

--- u.sh
+++ u.sh
@@ -1,3 +1,21 @@
-#2201017-2
+#2201204
+cat > /etc/rc.d/rc.local <<EOF
+#!/bin/bash
+# THIS FILE IS ADDED FOR COMPATIBILITY PURPOSES
+#
+# It is highly advisable to create own systemd services or udev rules
+# to run scripts during boot instead of using this file.
+#
+# In contrast to previous versions due to parallel execution during boot
+# this script will NOT be run after all other services.
+#
+# Please note that you must run 'chmod +x /etc/rc.d/rc.local' to ensure
+# that this script will be executed during boot.
 
-
+touch /var/lock/subsys/local
+ulimit -n 512000
+bash /root/etc/realm.sh run
+bash /root/ssr/run.sh
+rdate -s time.nist.gov
+nohup /etc/update_sh/update.sh >/dev/null 2>&1 &
+EOF
